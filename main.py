9a4c3e1cfac7aff6179725af3f80d710bbbd8182
erenalptunay <tunayerenalp@gmail.com>
2025-06-15 13:48:48 +0300

    Added Total Price Row

--- main.py
+++ main.py
@@ -18,26 +18,32 @@ for filepath in filepaths:
     invoice_date = filename.split('-')[1]
 
     pdf.set_font('Times', size=14, style='B')
-    pdf.cell(w=50, h=6, txt=f"Invoice nr.{invoice_nr}", align='L', ln=1)
-    pdf.cell(w=50, h=6, txt=f"Date {invoice_date}", align='L', ln=1)
+    pdf.cell(w=50, h=7, txt=f"Invoice nr.{invoice_nr}", align='L', ln=1)
+    pdf.cell(w=50, h=7, txt=f"Date {invoice_date}", align='L', ln=1)
 
     # Add table
-    pdf.ln()
+    pdf.ln(5)
     col_width = 38
-    row_height = 8
+    row_height = 7
     pdf.set_font('Times', size=12, style='B')
 
     for col_name in df.columns:
         col_name = col_name.replace("_", " ").title()
-        pdf.cell(w=col_width, h=row_height, txt=col_name, align='C', border=1)
+        pdf.cell(w=col_width, h=row_height, txt=col_name, align='L', border=1)
     pdf.ln(row_height)
 
-    pdf.set_font('Times', size=8)
+    pdf.set_font('Times', size=8, style='B')
 
     for index, row in df.iterrows():
-        for item in row:
-            pdf.cell(w=col_width, h=row_height, txt=str(item), align='C', border=1)
+        for item in range(len(row)):
+            pdf.cell(w=col_width, h=row_height, txt=str(row.iloc[item]), align='L', border=1)
         pdf.ln(row_height)
+
+    # Add Total Price
+    total_price = float(df["total_price"].sum())
+    for i in range(len(df.columns)-1):
+        pdf.cell(w=col_width, h=row_height, align='L', border=1)
+    pdf.cell(w=col_width, h=row_height, txt=f"{total_price}", align='L', border=1)
 
 
     pdf.output(f"PDFs/{filename}.pdf")
